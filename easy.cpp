7ebd611386981ce7159670cbb4a3112a97d33cf1
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-07-13 22:57:07 +0800

    Update easy.cpp

--- easy.cpp
+++ easy.cpp
@@ -1,27 +1,25 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-#define ar array
-
-void solve() {
+void test_case() {
 	string s, ns;
 	getline(cin, s);
-	for(int i=0; i<(int)s.size(); ++i) {
-		ns+=s[i];
-		if(s[i+1]==' '||s[i+1]=='\0') {
-			int tmp=stoi(ns);
-			int a[1000], cnt=0;
-			while(tmp!=0) {
-				a[cnt]=tmp%10;
-				tmp/=10;
+	for(int i = 0; i < (int)s.size(); ++i) {
+		ns += s[i];
+		if(s[i+1] == ' ' || s[i+1] == '\0') {
+			int tmp = stoi(ns);
+			int a[1000], cnt = 0;
+			while(tmp != 0) {
+				a[cnt] = tmp % 10;
+				tmp /= 10;
 				cnt++;
 			}
-			int d=0;
-			for(int j=cnt-1; j>=0; --j) {
-				d+=a[j]*pow(2, j);
+			int d = 0;
+			for(int j = cnt - 1; j >= 0; --j) {
+				d += a[j] * pow(2, j);
 			}
-			ns="";
+			ns = "";
 			cout << (char)d;
 		}
 	}
@@ -29,9 +27,14 @@ void solve() {
 }
 
 int main() {
-	ios::sync_with_stdio(0);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
 	
-	solve();
+	//int T;
+	//cin >> T;
+	//while(T--) {
+		//test_case();
+	//}
+	test_case();
 	return 0;
 }
